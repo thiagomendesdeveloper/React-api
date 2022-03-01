f42e71c6490e5ffa973f8b829e9212c7ee28d01d
thiagomendesdeveloper <thiagomendessilva77@gmail.com>
2022-03-01 10:16:04 -0300

    basic api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const [ repositories, setRepositories ] = useState([])
+
+  useEffect( ()=> {
+
+    axios.get('https://api.github.com/users/thiagomendesdeveloper/repos')
+    .then( response => {
+      setRepositories(response.data)
+    })
+
+  },[])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {repositories.map( repo => {
+        return(
+          <li key={repo.full_name}>
+            <strong>{repo.full_name}</strong>
+            <p>{repo.description}</p>
+          </li>
+        )
+      })}
+    </ul>
   );
+
 }
 
 export default App;
